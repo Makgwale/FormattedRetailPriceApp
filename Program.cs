fdfe0d960ca59e37abb1cd99fb5c9fd01eb889fc
Apple Lady <Apple Lady@DESKTOP-AFJ0G3M>
2023-03-03 00:24:14 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+	private static void Main(string[] args)
+	{
+		double openCollarRunningShirt = 41.00;
+
+		double markDownPrice = openCollarRunningShirt * 0.15;
+		double newPrice = openCollarRunningShirt - markDownPrice;
+
+		Console.WriteLine("You will be saving {0:C}",markDownPrice);
+		Console.WriteLine("The new price will be {0:C}", newPrice);
+
+	}
+}
